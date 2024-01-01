f227b9721dedbe5df10672b780bb7db1edd3d549
Tmh3101 <minhhieu31012004@gmail.com>
2024-01-01 14:07:56 +0700

    add Ex4 MTK

---
+++ Ex4/BaiTap8c&9c_MTK_da_vohuong.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#define MAX_N 100
+
+typedef struct {
+	int A[MAX_N][MAX_N];
+	int n, m;
+} Graph;
+
+void init_graph(Graph *pG, int n){
+	pG->n = n;
+	pG->m = 0;
+	int u, v;
+	for(u = 1; u <= n; u++){
+		for(v = 1; v <= n; v++){
+			pG->A[u][v] = 0;
+		}
+	}
+}
+
+void add_edge(Graph *pG, int u, int v){
+	if(u == v) pG->A[u][v] += 1;
+	else {
+		pG->A[u][v] += 1;
+		pG->A[v][u] += 1;
+	}
+	pG->m++;	
+}
+
+
+int main(){
+	Graph G;
+	int n, m;
+	scanf("%d%d", &n, &m);
+	init_graph(&G, n);
+	
+	int i, u, v;
+	for(i = 0; i < m; i++){
+		scanf("%d%d", &u, &v);
+		add_edge(&G, u, v);
+	}
+	
+	for(u = 1; u <= G.n; u++){
+		for(v = 1; v <= G.n; v++){
+			printf("%d ", G.A[u][v]);
+		}
+		printf("\n");
+	}
+	
+	
+	return 0;
+}
