7d53fe641d43ee03e9aaad86076500ad2762873f
Tmh3101 <minhhieu31012004@gmail.com>
2024-01-05 09:58:28 +0700

    add nang-cao

---
+++ Ex4/nang-cao.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#define MAX_N 100
+
+typedef struct {
+	int A[MAX_N][MAX_N];
+	int n, m;
+} Graph;
+
+void init_graph(Graph *pG, int n){
+	pG->n = n;
+	pG->m = 0;
+	int u, v;
+	for(u = 1; u <= n; u++){
+		for(v = 1; v <= n; v++){
+			pG->A[u][v] = 0;
+		}
+	}
+}
+
+void add_edge(Graph *pG, int u, int v){
+	if(u == v) {
+		pG->A[u][v] += 1;
+	} else {
+		pG->A[u][v] += 1;
+		pG->A[v][u] += 1;
+	}
+	pG->m++;	
+}
+
+int degree(Graph *pG, int u){
+	int deg = 0, i;
+	for(i = 1; i <= pG->n; i++){
+		deg += pG->A[u][i] + pG->A[i][u];
+	}
+	return deg + pG->A[u][u];
+}
+
+void neighbours(Graph *pG, int u){
+	int i;
+	for(i = 1; i <= pG->n; i++){
+		if(pG->A[u][i] != 0) printf(" %d", i);
+	}
+	printf("\n");
+}
+
+
+int main(int argc, char *argv[]){
+	Graph G;
+	
+	if(argc < 2){
+		printf("Hay go: %s <ten-file>\n", argv[0]);
+		return 1;
+	}
+	
+	
+	freopen(argv[1], "r", stdin);
+	int n, m;
+	scanf("%d%d", &n, &m);
+	init_graph(&G, n);
+	
+	int i, u, v;
+	for(i = 0; i < m; i++){
+		scanf("%d%d", &u, &v);
+		add_edge(&G, u, v);
+	}
+	
+	for(i = 1; i <= G.n; i++){
+		printf("neighbours(%d):", i);
+		neighbours(&G, i);
+	}
+	
+	return 0;
+}
